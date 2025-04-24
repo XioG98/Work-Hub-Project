206419cfa56781c052acd753af97f50e37926c28
XioG98 <xiogr98@gmail.com>
2025-04-23 21:06:13 -0500

    Landing page 90%, page contact_us, fixes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,40 +1,42 @@
-function openModal() {
-    document.getElementById('modal-login').style.display = 'flex';
-}
 
-function closeModal() {
-    document.getElementById('modal-login').style.display = 'none';
-}
-
-function clearFields() {
-    document.getElementById('usuario').value = "";
-    document.getElementById('password').value = "";
-}
 
 function openAdmin(){
-  if(email.value=='xio@gmail.com' && password.value=='123'){
-      window.open('admin.html')
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+  if(email.value=='admin@gmail.com' && password.value=='123'){
+      window.location='admin.html'
+      ;
   }
+  console.log("no")
+} 
 
-}
+  /* Carrusel jobs*/
+  document.addEventListener( 'DOMContentLoaded', function () {
+    new Splide( '#thumbnail-carousel', {
+      type   : 'loop',
 
+      fixedWidth : 250,
+      fixedHeight: 250,
+      arrows: false,
+      gap        : 50,
+      rewind     : true,
+      pagination : false,
+      focus      : 'start',
+      breakpoints: {
 
+        992: {
+          perPage: 4,
 
+        },
+        768: {
+          fixedWidth : 200,
+          fixedHeight: 200,
 
-function abrirModal(id) {
-    document.getElementById(id).style.display = 'block';
-    /*.style Accede a los estilos CSS en línea del elemento.*/
-    /* .display Modifica la propiedad display del elemento convirtiéndolo a bloque*/
-  }
-  
-  function cerrarModal(id) {
-    document.getElementById(id).style.display = 'none';
-    /*El valor none oculta el modal*/
-  }
-  
-  /* Carrusel historias*/
-document.addEventListener( 'DOMContentLoaded', function() {
-  var splide = new Splide( '.splide', {
+        },
+      },
+    } ).mount();  
+    
+    var splide = new Splide( '#ldg-pro', {
     type   : 'loop',
     perPage: 4,
    perMove: 2,
@@ -52,19 +54,37 @@ document.addEventListener( 'DOMContentLoaded', function() {
   } );
   
   splide.mount();
-} );
+  } );
 
 
-/* Icono mostrar/ocultar */
+/* Icono mostrar/ocultar login*/
 
 function togglePassword() {
   const input = document.getElementById("password");
   const icon = document.querySelector(".toggle-icon");
   if (input.type === "password") {
     input.type = "text";
-    icon.textContent = "visibility_off";
+    icon.innerHTML = `
+    <path d="M1 8.64235C12 18.5285 18.5 18.5285 28.5 8.64235" stroke="white" stroke-width="1.5" stroke-linecap="round"/>`;
   } else {
     input.type = "password";
-    icon.textContent = "visibility";
+    icon.innerHTML = `<path d="M2 9.11384C13 19 19.5 19 29.5 9.11384C18.8789 -1.37506 13.259 -2.02927 2 9.11384Z" stroke="white" stroke-width="1.5"/>
+                  <path d="M16 13.0284C18.4853 13.0284 20.5 11.0137 20.5 8.52844C20.5 6.04316 18.4853 4.02844 16 4.02844C13.5147 4.02844 11.5 6.04316 11.5 8.52844C11.5 11.0137 13.5147 13.0284 16 13.0284Z" fill="white" stroke="white"/>`;
+  }
+}
+
+/* Icono mostrar/ocultar signup*/
+
+function signupTogglePassword() {
+  const input = document.getElementById("loginPassword");
+  const icon = document.querySelector(".toggle-icon");
+  if (input.type === "password") {
+    input.type = "text";
+    icon.innerHTML = `
+    <path d="M1 8.64235C12 18.5285 18.5 18.5285 28.5 8.64235" stroke="white" stroke-width="1.5" stroke-linecap="round"/>`;
+  } else {
+    input.type = "password";
+    icon.innerHTML = `<path d="M2 9.11384C13 19 19.5 19 29.5 9.11384C18.8789 -1.37506 13.259 -2.02927 2 9.11384Z" stroke="white" stroke-width="1.5"/>
+                  <path d="M16 13.0284C18.4853 13.0284 20.5 11.0137 20.5 8.52844C20.5 6.04316 18.4853 4.02844 16 4.02844C13.5147 4.02844 11.5 6.04316 11.5 8.52844C11.5 11.0137 13.5147 13.0284 16 13.0284Z" fill="white" stroke="white"/>`;
   }
 }
